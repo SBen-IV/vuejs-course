2adf5e37c25a5b42de8cdf0d0072c453e9908686
SBen-IV <sinneo@fi.uba.ar>
2022-04-25 11:40:08 -0300

    Sidebar

--- product-and-cart-vue-cli/src/views/HomeView.vue
+++ product-and-cart-vue-cli/src/views/HomeView.vue
@@ -59,15 +59,10 @@
 </template>
 
 <script>
-import food from '../food.json'
 
 export default {
   name: 'HomeView',
-  data () {
-    return {
-      inventory: food
-    }
-  },
+  props: ['inventory'],
   components: {}
 }
 </script>
